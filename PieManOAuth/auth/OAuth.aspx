2faa670b70d837bfb5c25212a1f14a118b8a93fa
Nathan Woulfe <nathan@nathanw.com.au>
2015-06-01 16:50:45 +1000

    Initial commit

---
+++ PieManOAuth/auth/OAuth.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OAuth.aspx.cs" Inherits="Auth" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PieMan oAuth</title>
+        <link rel="stylesheet" href="/css/umbraco.css" />
+        
+        <style>
+
+            header {
+                background:#1d1d1d;
+                padding:15px 15px 7px;
+                margin-bottom:15px;
+            }
+
+            section {
+                padding:15px;
+            }
+
+            h1 {
+                margin:0;
+                color:#d9d9d9;
+            }
+
+            h1 span {
+                color:#df7f48;
+            }
+
+            h2 {
+                margin:-5px 0 0;
+                text-transform:uppercase;
+                font-size:18px;
+                color:#d9d9d9;
+            }
+
+            p:last-of-type {
+                margin-bottom:30px;
+            }
+            .btns {
+                text-align:right;
+                border-top:1px solid #eee;
+                padding-top:15px;
+                margin-top:15px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <form runat="server">
+            <div class="authcontainer">
+                <header>
+                    <h1>
+                        <span class="icon-pie-chart"></span>
+                        PieMan
+                    </h1>
+                    <h2>Simple analytics for Umbraco</h2>
+                </header>
+                <section>
+                    <p>For PieMan to deliver piping hot stats, it needs access to the Google Analytics account linked to this website.</p>
+                    <p>PieMan stores only the bare essentials to fill your analytics appetite.</p>
+                    <div class="btns">
+                        <asp:Hyperlink ID="link" Text="Sign in" runat="server" CssClass="btn btn-success" />
+                        <button class="btn" onclick="window.close()">Close</button>
+                    </div>
+                </section>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ PieManOAuth/auth/OAuth.aspx.cs
@@ -0,0 +1,79 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Auth : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var redirectUri = "http://pieman-auth.azurewebsites.net/auth/OAuth.aspx";
+
+        if (Request.QueryString["do"] == null && Request.QueryString["code"] == null)
+        {
+            Session["callback"] = Request.QueryString["clientcallback"];
+            Session["state"] = Request.QueryString["clientstate"];            
+            link.NavigateUrl = "?do=login&clientcallback=" + Session["callback"] + "&state=" + Session["state"];            
+        }
+
+        else if (Request.QueryString["do"] != null)
+        {
+            // The query string to send to the authentication site
+            NameValueCollection nvc = new NameValueCollection {
+                {"response_type", "code"},
+                {"client_id", "140468485447-dgk7rqv982gqk9ao9gbsck7epq0pu8ad.apps.googleusercontent.com"},
+                {"redirect_uri", redirectUri},
+                {"scope", "profile https://www.googleapis.com/auth/analytics.readonly"},
+                {"approval_prompt", "force"},
+                {"access_type","offline"}
+            };
+
+            var q = String.Join("&", nvc.AllKeys.Select(a => a + "=" + HttpUtility.UrlEncode(nvc[a])));
+
+            // Generate the URL for the authentication page
+            string oAuthUrl = "https://accounts.google.com/o/oauth2/auth?" + q;
+
+            Response.Redirect(oAuthUrl);
+        }
+
+        if (Request.QueryString["code"] != null)
+        {
+            //retreive 'code value' from query string.
+            var codeValue = Request["code"];
+
+            //If Google has sent us a 'code value' after authenticating the user,
+            //we will use this code to request an access token.
+            if (codeValue != null)
+            {
+                //Create a post request for to request an access token
+                WebClient client = new WebClient();
+
+                // creates the post data for the POST request
+                System.Collections.Specialized.NameValueCollection values = new System.Collections.Specialized.NameValueCollection();
+                values.Add("client_id", "140468485447-dgk7rqv982gqk9ao9gbsck7epq0pu8ad.apps.googleusercontent.com");
+                values.Add("client_secret", "90mdgzToSN9cc0H17Ic_9TsN");
+                values.Add("redirect_uri", redirectUri);
+                values.Add("code", codeValue);
+                values.Add("grant_type", "authorization_code");
+
+                // Post Request
+                byte[] response = client.UploadValues("https://accounts.google.com/o/oauth2/token", values);
+                var TokenInformation = System.Text.Encoding.UTF8.GetString(response);
+                dynamic TokenObject = JsonConvert.DeserializeObject(TokenInformation);
+
+                Response.Redirect(String.Format("{0}?state={1}&token={2}", Session["callback"].ToString(), Session["state"].ToString(), TokenObject.refresh_token));
+
+
+            }
+
+
+            Response.Redirect(Session["clientcallback"].ToString());
+        }
+    }
+}
